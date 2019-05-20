d02b08366f1de01ad4bb7b3299c77ce05552abfe
Najdanovszki <najdanovszki@gmail.com>
2019-05-20 21:41:32 +0200

    WEBService + html site to call the service

---
+++ SimpleWebService/SimpleWebService/AcceptFromMail.svc.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using SimpleWebService.Model;
+
+namespace SimpleWebService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class AcceptFromMail : IAcceptFromMail
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public AcceptModel SetAnswer(AcceptModel accept)
+        {
+            if (accept == null)
+            {
+                Debug.WriteLine("Valami nem jó");
+            } else
+            {
+                Debug.WriteLine("Siker");
+            }
+            return accept;
+        }
+    }
+}
---
+++ SimpleWebService/SimpleWebService/Model/AcceptModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleWebService.Model
+{
+    public class AcceptModel
+    {
+        public string answer { get; set; }
+
+    }
+}
